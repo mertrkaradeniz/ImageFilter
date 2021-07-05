e5e20e78978273b36c49e35a3a9123e15b785a91
Mert <mertrizakaradeniz@gmail.com>
2021-07-05 12:18:43 +0300

    Prepare image filters

--- app/src/main/java/com/example/imagefilter/data/repository/EditImageRepository.kt
+++ app/src/main/java/com/example/imagefilter/data/repository/EditImageRepository.kt
@@ -2,7 +2,9 @@ package com.example.imagefilter.data.repository
 
 import android.graphics.Bitmap
 import android.net.Uri
+import com.example.imagefilter.data.model.ImageFilter
 
 interface EditImageRepository {
     suspend fun prepareImagePreview(imageUri: Uri): Bitmap?
+    suspend fun getImageFilters(image: Bitmap): List<ImageFilter>
 }
--- app/src/main/java/com/example/imagefilter/ui/editimage/EditImageActivity.kt
+++ app/src/main/java/com/example/imagefilter/ui/editimage/EditImageActivity.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.imagefilter.adapters.ImageFiltersAdapter
 import com.example.imagefilter.databinding.ActivityEditImageBinding
 import com.example.imagefilter.ui.main.MainActivity
 import com.example.imagefilter.utils.displayToast
@@ -31,12 +32,25 @@ class EditImageActivity : AppCompatActivity() {
             binding.pbPreview.visibility =
                 if (dataState.isLoading) View.VISIBLE else View.GONE
             dataState.bitmap?.let { bitmap ->
-                binding.imgPreview.apply {
-                    setImageBitmap(bitmap)
-                    show()
-                }
+                binding.imgPreview.setImageBitmap(bitmap)
+                binding.imgPreview.show()
+                viewModel.loadImageFilters(bitmap)
             } ?: kotlin.run {
                 dataState.error?.let { error ->
+                    displayToast(error)
+                }
+            }
+        })
+        viewModel.imageFiltersUiState.observe(this, {
+            val imageFiltersDataState = it ?: return@observe
+            binding.pbImageFilters.visibility =
+                if (imageFiltersDataState.isLoading) View.VISIBLE else View.GONE
+            imageFiltersDataState.imageFilters?.let { imageFilters ->
+                ImageFiltersAdapter(imageFilters).also { adapter ->
+                    binding.rvFilters.adapter = adapter
+                }
+            } ?: kotlin.run {
+                imageFiltersDataState.error?.let { error ->
                     displayToast(error)
                 }
             }
